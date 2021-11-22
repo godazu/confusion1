a69e995458e47677ab67866efcca33749fd5f899
Goda Zukauskaite <goda.zukauskaite@mif.stud.vu.lt>
2021-11-22 20:18:32 +0200

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
@@ -9,7 +10,7 @@ import {DISHES} from './shared/dishes';
 import { COMMENTS } from './shared/comments';
 import { PROMOTIONS } from './shared/promotions';
 import { LEADERS } from './shared/leaders';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate} from 'react-router-dom';
 
 class Main extends Component {
   constructor(props){
@@ -35,13 +36,31 @@ class Main extends Component {
         />
       );
     }
+
+    const AboutUsPage = () => {
+      return(
+          <About 
+              leaders={this.state.leaders}
+          />
+      );
+  };
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return(
     <div>
       <Header/>
       <Routes>
         <Route path='/home' element={<Homepage/>} />
         <Route exact path='/menu' element={<Menu dishes={this.state.dishes}/>} />  
-        <Route exact path='/contactus' element={<Contact/>} />         
+        <Route path='/menu/:dishId' element={<DishWithId/>} />
+        <Route exact path='/contactus' element={<Contact/>} />
+        <Route exact path='/aboutus' element={<About leaders={this.state.leaders} />} />         
         <Route path="*" element={<Navigate to="/home" />}/>
       </Routes>   
       <Footer/>
