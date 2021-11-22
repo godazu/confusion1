822b726fb80a6c1bc9155c72d383603f275e82fb
Goda Zukauskaite <goda.zukauskaite@mif.stud.vu.lt>
2021-11-22 17:27:49 +0200

    React route

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from './shared/dishes';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props){
@@ -11,25 +13,29 @@ class Main extends Component {
 
     this.state={
       dishes: DISHES,
-      selectedDish: null
+
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
 
   render() {
+
+    const Homepage = () => {
+      return(
+        <Home/>
+      );
+    }
     return(
     <div>
       <Header/>
-      <Menu dishes={this.state.dishes} 
-           onClick={(dishId) => this.onDishSelect(dishId)} />
-      <DishDetail 
-           dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+      <Routes>
+        <Route path='/home' element={<Homepage/>} />
+        <Route exact path='/menu' element={<Menu dishes={this.state.dishes}/>} />            
+        <Route path="*" element={<Navigate to="/home" />}/>
+      </Routes>   
       <Footer/>
     </div>
-  );
+  );  
 }
 }
 
