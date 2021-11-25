7f604d230ec5f49dd3b27ddc841adc50e9f3cf46
Goda Zukauskaite <goda.zukauskaite@mif.stud.vu.lt>
2021-11-25 19:25:04 +0200

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -47,8 +47,9 @@ class Main extends Component {
 
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+        comments={this.props.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) }        
+        />  
       );
     };
 
