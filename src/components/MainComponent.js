f00979efae2e46443d81b245c3b6dad851f7e28e
Goda Zukauskaite <goda.zukauskaite@mif.stud.vu.lt>
2021-11-29 20:52:10 +0200

    Assignment 8

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, postFeedback, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -26,7 +26,10 @@ const mapDispatchToProps = (dispatch) => ({
   fetchDishes: () => { dispatch(fetchDishes()) },
   resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
   fetchComments: () => { dispatch(fetchComments()) },
-  fetchPromos: () => { dispatch(fetchPromos()) }
+  fetchPromos: () => { dispatch(fetchPromos()) },
+  fetchLeaders: () => dispatch(fetchLeaders()),
+  postFeedback: (firstname,lastname,telnum,email,agree,contactType,message) =>dispatch(postFeedback(firstname,
+        lastname,telnum,email,agree,contactType,message)),
 });
 
 class Main extends Component {
@@ -40,6 +43,7 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
 
   render() {
@@ -52,15 +56,20 @@ class Main extends Component {
           promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
           promosLoading={this.props.promotions.isLoading}
           promosErrMess={this.props.promotions.errMess}
-          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-        />
+          leader={this.props.leaders.leaders.filter(leader => leader.featured)[0] }
+          leaderLoading={this.props.leaders.isLoading}
+          leaderErrMess={this.props.leaders.errMess}       
+           />
       );
     }
 
     const AboutUsPage = () => {
       return (
-        <About
-          leaders={this.props.leaders}
+        <About 
+        leaders={this.props.leaders.leaders}
+        leaderLoading={this.props.leaders.isLoading}
+        leaderErrMess={this.props.leaders.errMess}
+
         />
       );
     };
@@ -87,7 +96,8 @@ class Main extends Component {
               <Route path="/home" component={HomePage} />
               <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
               <Route path="/menu/:dishId" component={DishWithId} />
-              <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+              <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} 
+                     postFeedback={this.props.postFeedback}/>} />
               <Route exact path="/aboutus" component={AboutUsPage} />
               <Redirect to="/home" />
             </Switch>
